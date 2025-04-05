9e4d2628a0fee291ed6112a9f3d811bc1210cf05
shihwen-vscode <shihwen@vscode.com>
2025-04-05 10:19:22 +0800

    [DS] update debuging information

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+terraform {
+  backend "gcs" {
+    bucket = "terraform-state-bucket--data-sharing"  # your bucket name
+    prefix = "terraform/state"                       # path inside the bucket
+  }
+}
+
 resource "google_storage_bucket" "my-bucket" {
   name                     = "test-githubdemo-bucket-001"
   project                  = "open-data-v2-cicd"
