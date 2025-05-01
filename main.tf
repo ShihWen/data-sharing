0e0e9e0164331188f3d823dc85100153508d624e
Shih-Wen WU (Peter) <shihwenwutw@gmail.com>
2025-05-01 16:05:03 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "my-bucket" {
-  name                     = "test-githubdemo-bucket-003" # You can change the bucket name
+  name                     = "test-githubdemo-bucket-004" # You can change the bucket name
   project                  = "open-data-v2-cicd" # REPLACE with your GCP Project ID, or use variable
   location                 = "ASIA-EAST1" # You can change the location/region
 
