5a1762abb1f2ce86063a91621fe40acabed0fafb
shihwen-vscode <shihwen@vscode.com>
2025-05-13 07:55:36 +0800

    Enhance Jenkinsfile and Terraform backend configuration for improved security and flexibility. Updated Jenkinsfile to include service account email in backend-config and streamlined access token handling. Modified backend.tf comments to clarify the use of environment-specific parameters for bucket and service account.

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,11 +1,10 @@
 terraform {
   backend "gcs" {
-    # Bucket name is environment-specific, so we'll pass it via -backend-config in Jenkinsfile
+    # Bucket name and service account are environment-specific, so we'll pass them via -backend-config in Jenkinsfile
     # bucket = var.terraform_state_bucket_name # THIS IS NOT ALLOWED IN BACKEND BLOCK
+    # storage_account will also be passed via -backend-config
 
     prefix = "terraform/state" # Shared path inside the bucket
     region = "asia-east1" # Set region here if it's the same for all environments
-
-    # service_account_email = "jenkins-cicd-dev@open-data-v2-cicd.iam.gserviceaccount.com" # Optional, can be set here or picked up from GCLOUD_AUTH_ACTIVATED
   }
 }
