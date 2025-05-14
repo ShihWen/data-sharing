df261c4b39414dba1476eeee045e12a055bfb0ca
shihwen-vscode <shihwen@vscode.com>
2025-05-14 11:17:50 +0800

    set up tables configuration

---
+++ terraform/bigquery_tables/main.tf
@@ -0,0 +1,41 @@
+locals {
+  table_files = fileset(path.module, "*/*/*.yaml")
+  table_configs = {
+    for file in local.table_files : file => yamldecode(file(file))
+  }
+}
+
+resource "google_bigquery_table" "tables" {
+  for_each = local.table_configs
+
+  dataset_id = var[each.value.dataset_id_var_name]
+  table_id   = each.value.table_id
+  project    = var.project_id
+  description = each.value.description
+
+  labels = each.value.labels
+
+  dynamic "time_partitioning" {
+    for_each = each.value.time_partitioning != null ? [each.value.time_partitioning] : []
+    content {
+      type  = time_partitioning.value.type
+      field = time_partitioning.value.field
+    }
+  }
+
+  dynamic "clustering" {
+    for_each = each.value.clustering != null ? [each.value.clustering] : []
+    content {
+      fields = clustering.value
+    }
+  }
+
+  schema = jsonencode([
+    for field in each.value.schema : {
+      name        = field.name
+      type        = field.type
+      mode        = field.mode
+      description = field.description
+    }
+  ])
+} 
---
+++ terraform/bigquery_tables/variables.tf
@@ -0,0 +1,14 @@
+ variable "project_id" {
+  description = "The ID of the project where tables will be created"
+  type        = string
+}
+
+variable "data_sharing_dataset_id" {
+  description = "The ID of the data sharing dataset"
+  type        = string
+}
+
+variable "analytics_dataset_id" {
+  description = "The ID of the analytics dataset"
+  type        = string
+}
