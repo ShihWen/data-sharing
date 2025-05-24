224e854d6b1c3379e83efe5bd585cbf3a816f93e
shihwen-vscode <shihwen@vscode.com>
2025-05-24 16:48:15 +0800

    Add new test DAG for BigQuery integration
    - Introduced 'dag_detection_test_afternoon', featuring a simple print task and an example BigQuery query task.
    - Scheduled to run daily at noon, this DAG serves as a demonstration of BigQuery integration within Airflow.

---
+++ terraform/airflow/docker/dags/test_dag_detection-3.py
@@ -0,0 +1,59 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryExecuteQueryOperator
+import os
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+def print_hello():
+    """Print hello as a test task"""
+    print('Hello from Airflow, I\'m uploaded by Jenkins at 2025-05-24 16:42')
+    return 'I\'m uploaded by Jenkins!'
+
+with DAG(
+    'dag_detection_test_afternoon',
+    default_args=default_args,
+    description='A simple example DAG with BigQuery integration',
+    schedule_interval='0 12 * * *',
+    start_date=datetime(2024, 1, 1),
+    catchup=False,
+    tags=['example'],
+) as dag:
+
+    start = EmptyOperator(
+        task_id='start',
+    )
+
+    hello_task = PythonOperator(
+        task_id='hello_task',
+        python_callable=print_hello,
+    )
+
+    # Example BigQuery task
+    bq_task = BigQueryExecuteQueryOperator(
+        task_id='example_bq_query',
+        sql='''
+            SELECT * 
+            FROM `open-data-v2-cicd.tpe_mrt_bronze.mrt_exit`
+            LIMIT 10
+        ''',
+        use_legacy_sql=False,
+        location='asia-east1',
+        gcp_conn_id='google_cloud_default'
+    )
+
+    end = EmptyOperator(
+        task_id='end',
+    )
+
+    # Define task dependencies
+    start >> hello_task >> bq_task >> end 
