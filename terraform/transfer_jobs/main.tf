b2e634d3e203aa3ca7d2792b2ddbd586bfa08b74
PeterOffice <Peter.Wu@office.com>
2025-06-19 14:55:19 +0800

    remove transfer jobs temporarily

--- terraform/transfer_jobs/main.tf
+++ terraform/transfer_jobs/main.tf
@@ -51,52 +51,52 @@ resource "google_secret_manager_secret_version" "aws_credentials_version" {
 }
 
 # Transfer job for MRT Traffic data
-resource "google_bigquery_data_transfer_config" "mrt_traffic_transfer" {
-  depends_on = [google_project_service.enable_transfer, google_project_iam_member.transfer_sa_roles]
+# resource "google_bigquery_data_transfer_config" "mrt_traffic_transfer" {
+#   depends_on = [google_project_service.enable_transfer, google_project_iam_member.transfer_sa_roles]
 
-  display_name           = "MRT Traffic Data Transfer"
-  project               = var.project_id
-  location              = "asia-east1"
-  data_source_id        = "amazon_s3"
-  schedule              = var.schedule
-  destination_dataset_id = var.dataset_ids["tpe_mrt_bronze"]
-  service_account_name  = google_service_account.transfer_sa.email
-  disabled              = false
+#   display_name           = "MRT Traffic Data Transfer"
+#   project               = var.project_id
+#   location              = "asia-east1"
+#   data_source_id        = "amazon_s3"
+#   schedule              = var.schedule
+#   destination_dataset_id = var.dataset_ids["tpe_mrt_bronze"]
+#   service_account_name  = google_service_account.transfer_sa.email
+#   disabled              = false
 
-  params = {
-    destination_table_name_template = "mrt_traffic"
-    data_path                      = "s3://${var.s3_bucket}/mrt-traffic/*.parquet"
-    access_key_id                  = var.aws_access_key
-    secret_access_key              = var.aws_secret_key
-    file_format                    = "PARQUET"
-    max_bad_records               = 0
-    write_disposition             = "WRITE_APPEND"
-  }
-}
+#   params = {
+#     destination_table_name_template = "mrt_traffic"
+#     data_path                      = "s3://${var.s3_bucket}/mrt-traffic/*.parquet"
+#     access_key_id                  = var.aws_access_key
+#     secret_access_key              = var.aws_secret_key
+#     file_format                    = "PARQUET"
+#     max_bad_records               = 0
+#     write_disposition             = "WRITE_APPEND"
+#   }
+# }
 
 # Transfer job for MRT Station data
-resource "google_bigquery_data_transfer_config" "mrt_station_transfer" {
-  depends_on = [google_project_service.enable_transfer, google_project_iam_member.transfer_sa_roles]
+# resource "google_bigquery_data_transfer_config" "mrt_station_transfer" {
+#   depends_on = [google_project_service.enable_transfer, google_project_iam_member.transfer_sa_roles]
 
-  display_name           = "MRT Station Data Transfer"
-  project               = var.project_id
-  location              = "asia-east1"
-  data_source_id        = "amazon_s3"
-  schedule              = var.schedule
-  destination_dataset_id = var.dataset_ids["tpe_mrt_bronze"]
-  service_account_name  = google_service_account.transfer_sa.email
-  disabled              = false
+#   display_name           = "MRT Station Data Transfer"
+#   project               = var.project_id
+#   location              = "asia-east1"
+#   data_source_id        = "amazon_s3"
+#   schedule              = var.schedule
+#   destination_dataset_id = var.dataset_ids["tpe_mrt_bronze"]
+#   service_account_name  = google_service_account.transfer_sa.email
+#   disabled              = false
 
-  params = {
-    destination_table_name_template = "mrt_station"
-    data_path                      = "s3://${var.s3_bucket}/mrt-station/mrt_station*"
-    access_key_id                  = var.aws_access_key
-    secret_access_key              = var.aws_secret_key
-    file_format                    = "PARQUET"
-    max_bad_records               = 0
-    write_disposition             = "WRITE_APPEND"
-  }
-}
+#   params = {
+#     destination_table_name_template = "mrt_station"
+#     data_path                      = "s3://${var.s3_bucket}/mrt-station/mrt_station*"
+#     access_key_id                  = var.aws_access_key
+#     secret_access_key              = var.aws_secret_key
+#     file_format                    = "PARQUET"
+#     max_bad_records               = 0
+#     write_disposition             = "WRITE_APPEND"
+#   }
+# }
 
 # Transfer job for MRT Exit data
 resource "google_bigquery_data_transfer_config" "mrt_exit_transfer" {
