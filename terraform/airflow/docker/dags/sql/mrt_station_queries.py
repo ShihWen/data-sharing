06aa90bca91416bb43f1f046491c1f5fb7d6d48a
PeterOffice <Peter.Wu@office.com>
2025-06-20 15:21:20 +0800

    Refactor MRT station SQL and BigQuery schema for improved clarity
    - Updated the SQL query in `mrt_station_queries.py` to reorder fields in the STRUCT for better readability.
    - Removed unused fields `city_code` and `town_code` from the BigQuery schema in `mrt_station_silver.yaml`, streamlining the data structure and enhancing clarity.

--- terraform/airflow/docker/dags/sql/mrt_station_queries.py
+++ terraform/airflow/docker/dags/sql/mrt_station_queries.py
@@ -70,11 +70,9 @@ WHEN NOT MATCHED THEN
     S.StationAddress,
     -- STRUCT for location details
     STRUCT(
-      S.parsed_postal_code AS postal_code,
-      S.parsed_city AS city,
-      CAST(NULL AS STRING) AS city_code, -- No source for city_code
+      S.parsed_city AS city,  
       S.parsed_town AS town,
-      CAST(NULL AS STRING) AS town_code, -- No source for town_code
+      S.parsed_postal_code AS postal_code,
       S.parsed_street_address AS street_address
     ),
     -- Create GEOGRAPHY point from lat/lon
