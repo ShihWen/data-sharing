6fd3a22642ba0c8cf9cf436969f331351b0ebde0
shihwen-vscode <shihwen@vscode.com>
2025-06-21 15:36:19 +0800

    Update Terraform configurations and script permissions
    - Added scheduling parameters for MRT data transfers in the Terraform configuration to automate job execution.
    - Updated the permissions of `setup_auto_connections.sh` to make it executable, enhancing script usability.

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,18 +39,21 @@ locals {
       destination_table   = "mrt_traffic"
       data_path           = "s3://${var.s3_bucket}/mrt-traffic/*.parquet"
       destination_dataset = "tpe_mrt_bronze"
+      schedule            = "every saturday 00:30"
     }
     mrt_station = {
       display_name        = "MRT Station Data Transfer"
       destination_table   = "mrt_station"
       data_path           = "s3://${var.s3_bucket}/mrt-station/mrt_station*"
       destination_dataset = "tpe_mrt_bronze"
+      schedule            = "every saturday 01:00"
     }
     mrt_exit = {
       display_name        = "MRT Exit Data Transfer"
       destination_table   = "mrt_exit"
       data_path           = "s3://${var.s3_bucket}/mrt-station/mrt_exit*"
       destination_dataset = "tpe_mrt_bronze"
+      schedule            = "every saturday 01:30"
     }
   }
 }
@@ -142,6 +145,7 @@ module "transfer_jobs" {
   data_path_uri                  = each.value.data_path
   destination_dataset_id         = local.dataset_outputs[each.value.destination_dataset]
   transfer_service_account_email = google_service_account.transfer_sa.email
+  schedule                       = each.value.schedule
 
   depends_on = [
     module.bigquery_datasets,
