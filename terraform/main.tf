cc22776ddc3f0f7ae3e622beeb774caf11c30db3
PeterOffice <Peter.Wu@office.com>
2025-06-30 14:14:32 +0800

    Add required provider configuration for Google in Terraform
    - Specified the Google provider with version constraint in the Terraform configuration.
    - Ensured compatibility with the required Terraform version.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = ">= 1.11.3"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.3"
+    }
+  }
 }
 
 provider "google" {
