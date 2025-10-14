98fc839f1cc4d696de0c6759ffc468e83d597d1b
PeterOffice <Peter.Wu@office.com>
2025-10-14 15:15:42 +0800

    Implement merge and insert operations for city bus shape data in the DAG, replacing previously commented-out tasks with active BigQueryInsertJobOperator instances for merging and inserting records. This enhances the data pipeline by ensuring proper data flow into BigQuery.

--- terraform/airflow/docker/dags/tdx_city_bus_shape_source_to_silver.py
+++ terraform/airflow/docker/dags/tdx_city_bus_shape_source_to_silver.py
@@ -14,7 +14,7 @@ from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobO
 
 from utils.bus_processing import process_inter_city_bus_shape
 from utils.tdx_api import get_tdx_data
-from sql.bus_queries import MERGE_SCD2_INTERCITY_BUS_ROUTE, INSERT_UPDATED_INTERCITY_BUS_ROUTE
+from sql.bus_queries import MERGE_SCD2_CITY_BUS_ROUTE, INSERT_UPDATED_CITY_BUS_ROUTE
 
 def fetch_bus_shape_to_gcs(**context):
     """
@@ -163,34 +163,34 @@ with DAG(
         python_callable=process_bus_shape_to_staging,
     )
     
-    # merge_into_silver_scd2 = BigQueryInsertJobOperator(
-    #     task_id="merge_into_silver_scd2",
-    #     configuration={
-    #         "query": {
-    #             "query": MERGE_SCD2_INTERCITY_BUS_ROUTE.format(
-    #                 project_id="{{ var.value.gcp_project_id }}",
-    #                 dataset_id="bus_silver",
-    #                 table_id="city_bus_shape",
-    #                 staging_table_id="city_bus_shape_staging",
-    #             ),
-    #             "useLegacySql": False,
-    #         }
-    #     },
-    # )
+    merge_into_silver_scd2 = BigQueryInsertJobOperator(
+        task_id="merge_into_silver_scd2",
+        configuration={
+            "query": {
+                "query": MERGE_SCD2_CITY_BUS_ROUTE.format(
+                    project_id="{{ var.value.gcp_project_id }}",
+                    dataset_id="bus_silver",
+                    table_id="city_bus_shape",
+                    staging_table_id="city_bus_shape_staging",
+                ),
+                "useLegacySql": False,
+            }
+        },
+    )
 
-    # insert_updated_records = BigQueryInsertJobOperator(
-    #     task_id="insert_updated_records",
-    #     configuration={
-    #         "query": {
-    #             "query": INSERT_UPDATED_INTERCITY_BUS_ROUTE.format(
-    #                 project_id="{{ var.value.gcp_project_id }}",
-    #                 dataset_id="bus_silver",
-    #                 table_id="inter_city_bus_shape",
-    #                 staging_table_id="inter_city_bus_shape_staging",
-    #             ),
-    #             "useLegacySql": False,
-    #         }
-    #     },
-    # )
+    insert_updated_records = BigQueryInsertJobOperator(
+        task_id="insert_updated_records",
+        configuration={
+            "query": {
+                "query": INSERT_UPDATED_CITY_BUS_ROUTE.format(
+                    project_id="{{ var.value.gcp_project_id }}",
+                    dataset_id="bus_silver",
+                    table_id="city_bus_shape",
+                    staging_table_id="city_bus_shape_staging",
+                ),
+                "useLegacySql": False,
+            }
+        },
+    )
 
-    fetch_bronze_data >> process_silver_staging #>> merge_into_silver_scd2 >> insert_updated_records 
+    fetch_bronze_data >> process_silver_staging >> merge_into_silver_scd2 >> insert_updated_records 
