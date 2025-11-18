d93417812142c0ecdc5b342fb275f4860ab1d57f
PeterOffice <Peter.Wu@office.com>
2025-11-18 14:00:38 +0800

    Add new DAG for fetching and processing intercity bus stop data from TDX API to GCS and staging in BigQuery. Includes functions for data retrieval, transformation, and loading into the silver dataset, enhancing the data pipeline for intercity bus systems.

--- terraform/airflow/docker/dags/utils/bus_processing.py
+++ terraform/airflow/docker/dags/utils/bus_processing.py
@@ -131,3 +131,99 @@ def process_inter_city_bus_station(raw_data: str) -> pd.DataFrame:
     df = pd.DataFrame(processed_records)
 
     return df
+
+def process_inter_city_bus_stop_of_route(raw_data: str) -> pd.DataFrame:
+    """
+    Transforms the raw JSON string for inter city bus station into a clean DataFrame,
+    handling nested structures for BigQuery ingestion.
+    """
+    stop_of_route_data = json.loads(raw_data)
+    
+    processed_records = []
+    current_timestamp = pendulum.now('UTC')
+    valid_to_ts_default = '9999-12-31T23:59:59Z'
+    
+    for feature in stop_of_route_data:
+        route_uid = feature.get('RouteUID', {})
+        route_id = feature.get('RouteID', {})
+        
+        route_name_dict = feature.get('RouteName', {})
+        route_name_zh_tw = route_name_dict.get('Zh_tw', '')
+        route_name_en = route_name_dict.get('En', '')
+        
+        operator_dict = feature.get('Operators', {})
+        operator_id = operator_dict.get('OperatorID', '')
+        operator_name_dict = operator_dict.get('OperatorName', {})
+        operator_name_zh_tw = operator_name_dict.get('Zh_tw', '')
+        operator_name_en = operator_name_dict.get('En', '')
+        operator_code = operator_dict.get('OperatorCode', '')
+        operator_no = operator_dict.get('OperatorNo', '')
+
+        sub_route_uid = feature.get('SubRouteUID', {})
+        sub_route_id = feature.get('SubRouteID', {})
+        sub_route_name_dict = feature.get('SubRouteName', {})
+        sub_route_name_zh_tw = sub_route_name_dict.get('Zh_tw', '')
+        sub_route_name_en = sub_route_name_dict.get('En', '')
+        direction = feature.get('Direction', '')
+        
+        stops_dict = feature.get('Stops', {})
+        processed_stops = []
+        for stop in stops_dict:
+            stop_name_dict = stop.get('StopName', {})
+            stop_id = stop.get('StopID', '')
+            stop_name_zh_tw = stop_name_dict.get('Zh_tw', '')
+            stop_name_en = stop_name_dict.get('En', '')
+
+            stop_position_dict = stop.get('StopPosition', {})
+            stop_position_lon = stop_position_dict.get('PositionLon', '')
+            stop_position_lat = stop_position_dict.get('PositionLat', '')
+            stop_position_geohash = stop_position_dict.get('GeoHash', '')
+            processed_stops.append({
+                'stop_uid': stop.get('StopUID'),
+                'stop_id': stop.get('StopID'),
+                'stop_name': {
+                    'zh_tw': stop_name_dict.get('Zh_tw'),
+                    'en': stop_name_dict.get('En', '')
+                },
+                'stop_position': {
+                    'position_lon': stop_position_lon,
+                    'position_lat': stop_position_lat,
+                    'geohash': stop_position_geohash
+                },
+                'station_id': stop.get('StationID'),
+                'station_group_id': stop.get('StationGroupID'),
+                'location_city_code': stop.get('LocationCityCode')
+            })
+
+        record = {
+            'route_uid': route_uid,
+            'route_id': route_id,
+            'route_name': {
+                'zh_tw': route_name_zh_tw,
+                'en': route_name_en
+            },
+            'operators': {
+                'operator_id': operator_id,
+                'operator_name': {
+                    'zh_tw': operator_name_zh_tw,
+                    'en': operator_name_en
+                },
+                'operator_code': operator_code,
+                'operator_no': operator_no
+            },
+            'sub_route_uid': sub_route_uid,
+            'sub_route_id': sub_route_id,
+            'sub_route_name': {
+                'zh_tw': sub_route_name_zh_tw,
+                'en': sub_route_name_en
+            },
+            'direction': direction,
+            'stops': processed_stops,
+            'update_time': feature.get('UpdateTime'),
+            'version_id': feature.get('VersionID'),
+        }
+        processed_records.append(record)
+        
+    df = pd.DataFrame(processed_records)
+
+    return df
